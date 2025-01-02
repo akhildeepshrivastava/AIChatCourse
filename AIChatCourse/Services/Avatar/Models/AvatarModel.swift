3b3c4cb7454084fcbdd3ee947845a66325b6cb92
Akhil <akhildeep.shrivastava@gmail.com>
2025-01-01 21:13:04 -0800

    [Feature] AvatarModel

---
+++ AIChatCourse/Services/Avatar/Models/AvatarModel.swift
@@ -0,0 +1,98 @@
+//
+//  Untitled.swift
+//  AIChatCourse
+//
+//  Created by Akhildeep Shrivastava on 1/1/25.
+//
+
+import Foundation
+
+struct AvatarModel {
+    let avatarId: String
+    let name: String?
+    let characterCategory: CharacterCategory?
+    let characterAction: CharacterAction?
+    let characterLocation: CharacterLocation?
+    let profileImageName: String?
+    let authorId: String?
+    let dateCreated: Date?
+    
+    init(
+        avatarId: String,
+        name: String? = nil,
+        characterCategory: CharacterCategory? = nil,
+        characterAction: CharacterAction? = nil,
+        characterLocation: CharacterLocation? = nil,
+        profileImageName: String? = nil,
+        authorId: String? = nil,
+        dateCreated: Date? = nil
+    ) {
+        self.avatarId = avatarId
+        self.name = name
+        self.characterCategory = characterCategory
+        self.characterAction = characterAction
+        self.characterLocation = characterLocation
+        self.profileImageName = profileImageName
+        self.authorId = authorId
+        self.dateCreated = dateCreated
+    }
+    
+    var characterDescription: String {
+        AvatarDescription(avatar: self).characterDescription
+    }
+    
+    static var mock: AvatarModel {
+        mocks[0]
+    }
+    
+    static var mocks: [AvatarModel] {
+        [
+            AvatarModel(avatarId: UUID().uuidString, name: "Alpha", characterCategory: .alien, characterAction: .smiling, characterLocation: .park, profileImageName: Constants.randomImage, authorId: UUID().uuidString, dateCreated: .now),
+            AvatarModel(avatarId: UUID().uuidString, name: "Beta", characterCategory: .dog, characterAction: .eating, characterLocation: .mall, profileImageName: Constants.randomImage, authorId: UUID().uuidString, dateCreated: .now),
+            AvatarModel(avatarId: UUID().uuidString, name: "Gamma", characterCategory: .cat, characterAction: .shopping, characterLocation: .forest, profileImageName: Constants.randomImage, authorId: UUID().uuidString, dateCreated: .now),
+            AvatarModel(avatarId: UUID().uuidString, name: "delta", characterCategory: .woman, characterAction: .relaxing, characterLocation: .museum, profileImageName: Constants.randomImage, authorId: UUID().uuidString, dateCreated: .now)
+            
+        ]
+    }
+}
+
+struct AvatarDescription {
+    let category: CharacterCategory
+    let action: CharacterAction
+    let location: CharacterLocation
+    
+    init(avatar: AvatarModel) {
+        self.category = avatar.characterCategory ?? .default
+        self.action = avatar.characterAction ?? .default
+        self.location = avatar.characterLocation ?? .default
+    }
+    
+    init(category: CharacterCategory, action: CharacterAction, location: CharacterLocation) {
+        self.category = category
+        self.action = action
+        self.location = location
+    }
+    
+    var characterDescription: String {
+        "A \(category.rawValue) that is \(action.rawValue)  in the \(location.rawValue)"
+    }
+}
+
+enum CharacterCategory: String {
+    case man, woman, alien, dog, cat
+    
+    static var `default`: Self { .man }
+}
+
+enum CharacterAction: String {
+    case smiling, sittinmg, eating, drinking, walking, shopping, studying, working, relaxing, fighting, crying
+    
+    static var `default`: Self { .smiling }
+}
+
+enum CharacterLocation: String{
+    case park, mall, museum, city, forest, desert, space
+    
+    static var `default`: Self { .park }
+
+}
